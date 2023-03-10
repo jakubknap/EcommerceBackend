042faa7d760152bc3dad637a6b792da292631855
Jakub Knap <jknap40@gmail.com>
2023-03-10 15:14:54 +0100

    Pobieranie produktów z bazy danych

---
+++ src/main/java/pl/knap/shop/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package pl.knap.shop.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.knap.shop.product.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
